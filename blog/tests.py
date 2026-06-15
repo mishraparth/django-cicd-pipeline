aa57869355bf0420886fbf960f9ca0b7eaafe3e7
mishraparth <mishraparth3@gmail.com>
2026-06-15 13:38:33 +0530

    i added some lines in tests for print

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-
+print("hello to everyone")
 class PageTests(TestCase):
     def test_home_page(self):
         response = self.client.get(reverse('home'))
@@ -13,3 +13,6 @@ class PageTests(TestCase):
     def test_posts_page(self):
         response = self.client.get(reverse('posts'))
         self.assertEqual(response.status_code, 200)
+
+print("now the tests have been  done")
+print("this is some random text by parth ")
